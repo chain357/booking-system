53d4f3374e8453b65b0454a3789bafab38976f86
chain357 <chain6868@gmail.com>
2025-03-08 13:28:23 +0000

    ต่อ

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    aprUrl: "https://script.google.com/macros/s/AKfycbxVUinvWzsp178oiD7ldBJFlc3J-Bke1377PsJ9uuBIbrKgI-TlxuO0xtzmqSV3af0b/exec"
+    aprUrl: "https://script.google.com/macros/s/AKfycbx-I0hJ_Aa6vwk7AEIApf4hwQ4he7EL1hPUex3U6JpygjblT3dDrXeXyaKYDUPqIZMfKA/exec"
 }
